2f2b24f8b6d7fa19da7ebe34c65d30fb5176c6ef
DurgaPrasadKattunga <prasadkattunga50@gmail.com>
2025-09-27 19:51:20 +0530

    'hello'

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: true,                     // enable SSL/TLS
-      tlsAllowInvalidCertificates: true // only for local development
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
